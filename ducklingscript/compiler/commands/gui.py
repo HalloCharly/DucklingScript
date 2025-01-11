ac196593d5bed68376ee1f82f44667d8041b04cf
HalloCharly <149373315+HalloCharly@users.noreply.github.com>
2025-01-11 01:04:53 +0100

    Update gui.py
    ducklingscript/compiler/commands/gui.py
    
    Extended the possible parameters for the "GUI" command

--- ducklingscript/compiler/commands/gui.py
+++ ducklingscript/compiler/commands/gui.py
@@ -8,10 +8,14 @@ As if the user was to press the windows/meta key.
 class Gui(SimpleCommand):
     names = ["GUI", "WINDOWS", "META"]
     description = desc
-
+    parameters = ["TAB", "DOWNARROW", "UPARROW", "LEFTARROW", "RIGHTARROW", "PRINT"] #All valid arguments to the Windows keys that are not 1 character(s) long
+    
     def verify_arg(self, arg: ArgLine) -> str | None:
-        return (
-            None
-            if len(arg) == 1
-            else "Only one character is required. No more or less."
-        )
+        i = arg.content
+        if i.upper() in self.parameters:
+            return None
+        elif len(i) == 1:
+            return None
+        else:
+           return "Only one character is required. No more or less."
+        
